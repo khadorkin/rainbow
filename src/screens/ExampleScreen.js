be824e58c115313c32e73e047ecc6864c64b6e7c
Bruno Barbieri <brunobar79@gmail.com>
2020-02-11 22:41:18 -0500

    unlock and swap working

--- src/screens/ExampleScreen.js
+++ src/screens/ExampleScreen.js
@@ -1,10 +1,22 @@
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
+import { Text } from 'react-native';
 import { compose } from 'recompact';
-import { GasSpeedButton } from '../components/gas';
+import Button from '../components/buttons/Button';
 import { Centered, Page } from '../components/layout';
-import { withDataInit, withAccountData } from '../hoc';
+import {
+  withDataInit,
+  withAccountData,
+  withUniswapAssets,
+  withGas,
+  withUniswapAllowances,
+  withBlockPolling,
+} from '../hoc';
 import { colors, position } from '../styles';
+import swapOnUniswap from '../raps/swap-uniswap';
+import { loadWallet } from '../model/wallet';
+import { gasUtils } from '../utils';
+import { get } from 'lodash';
 
 class ExampleScreen extends PureComponent {
   static propTypes = {
@@ -14,8 +26,61 @@ class ExampleScreen extends PureComponent {
   componentDidMount = async () => {
     try {
       await this.props.initializeWallet();
+      this.props.gasPricesStartPolling();
+      this.props.web3ListenerInit();
     } catch (error) {
       console.log('lol error on ExampleScreen like a n00b: ', error);
+    }
+  };
+
+  componentWillUnmount() {
+    this.props.web3ListenerStop();
+    this.props.gasPricesStopPolling();
+  }
+
+  doSwap = async () => {
+    const wallet = await loadWallet();
+
+    const { gasPrices, inputReserve, outputReserve } = this.props;
+
+    const inputCurrency = {
+      address: '0x6b175474e89094c44da98b954eedeac495271d0f',
+      decimals: 18,
+      exchangeAddress: '0x2a1530c4c41db0b0b2bb646cb5eb1a67b7158667',
+    };
+    const outputCurrency = {
+      address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+      decimals: 18,
+    };
+
+    await this.props.uniswapUpdateInputCurrency(inputCurrency);
+    await this.props.uniswapUpdateOutputCurrency(outputCurrency);
+    await this.props.web3UpdateReserves();
+
+    const inputAmount = 0.1; // DAI
+    const outputAmount = 0.0004; // WETH
+    const selectedGasPrice = get(gasPrices, `[${gasUtils.NORMAL}]`);
+    const inputAsExactAmount = true;
+
+    try {
+      const swap = await swapOnUniswap(
+        wallet,
+        inputCurrency,
+        outputCurrency,
+        inputAmount,
+        outputAmount,
+        selectedGasPrice,
+        gasPrices,
+        inputAsExactAmount,
+        inputReserve,
+        outputReserve
+      );
+
+      console.log('SWAP EXECUTED!', swap.hash);
+      await swap.wait();
+      console.log('SWAP CONFIRMED');
+    } catch (e) {
+      console.log('SWAP FAILED', e);
     }
   };
 
@@ -26,16 +91,20 @@ class ExampleScreen extends PureComponent {
       color={colors.dark}
       flex={1}
     >
-      {/*
-        // haha you can put stuff here if you wanna test a component in isolation!
-        // ... i dont want to set up storybook right now
-
-      */}
       <Centered width="100%">
-        <GasSpeedButton flex={1} />
+        <Button onPress={this.doSwap}>
+          <Text>Swap</Text>
+        </Button>
       </Centered>
     </Page>
   );
 }
 
-export default compose(withAccountData, withDataInit)(ExampleScreen);
+export default compose(
+  withAccountData,
+  withDataInit,
+  withUniswapAssets,
+  withUniswapAllowances,
+  withGas,
+  withBlockPolling
+)(ExampleScreen);
