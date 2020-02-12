474a5b9c57d114a7552d0b74491433503a1560a8
Bruno Barbieri <brunobar79@gmail.com>
2020-02-12 13:02:26 -0500

    added reducer

--- src/screens/Routes/Routes.ios.js
+++ src/screens/Routes/Routes.ios.js
@@ -163,7 +163,7 @@ const MainNavigator = createStackNavigator(
       onTransitionStart,
     },
     headerMode: 'none',
-    initialRouteName: 'SwipeLayout',
+    initialRouteName: 'ExampleScreen',
     mode: 'modal',
   }
 );
